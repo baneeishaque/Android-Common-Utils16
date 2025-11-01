d3388a31383a6dfe6b2b9c6c2054a5a08e56b0b6
Banee Ishaque K <baneeishaque@gmail.com>
2025-11-01 23:16:50 +0530

    Improves code readability
    Adds blank lines for better visual separation within method bodies and constructor.

--- common16/src/main/java/ndk/utils_android16/UpdateUtilsWrapperBase.java
+++ common16/src/main/java/ndk/utils_android16/UpdateUtilsWrapperBase.java
@@ -9,14 +9,17 @@ public class UpdateUtilsWrapperBase {
     private static String applicationName;
 
     UpdateUtilsWrapperBase(String applicationName) {
+
         UpdateUtilsWrapperBase.applicationName = applicationName;
     }
 
     public static String[] getFlavouredServerVersion(String flavour, String fullVersionCheckUrl, Context currentApplicationContext, boolean isGuiAvailable) {
+
         return UpdateUtils.getFlavouredServerVersion(flavour, fullVersionCheckUrl, applicationName, currentApplicationContext, isGuiAvailable);
     }
 
     public static String[] getServerVersion(String fullVersionCheckUrl, Context currentApplicationContext, boolean isGuiAvailable) {
+
         return UpdateUtils.getServerVersion(fullVersionCheckUrl, applicationName, currentApplicationContext, isGuiAvailable);
     }
 }
