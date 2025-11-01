9763a1c789a24d9c9b43e02f5757a2a6217386b5
Banee Ishaque K <baneeishaque@gmail.com>
2025-11-01 19:18:34 +0530

    Adopts AlertDialogUtils for update prompt
    Refactors the application update dialog to utilize the `AlertDialogUtils` class. This change removes direct `AlertDialog` creation, improving consistency and reusability of dialogs across the application.

--- common16/src/main/java/ndk/utils_android16/update/UpdateApplication.java
+++ common16/src/main/java/ndk/utils_android16/update/UpdateApplication.java
@@ -1,20 +1,17 @@
 package ndk.utils_android16.update;
 
-import android.app.AlertDialog;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import ndk.utils_android14.ApplicationVCSUtils;
+import ndk.utils_android16.AlertDialogUtils;
 
 public class UpdateApplication {
 
     public static void updateApplication(final String applicationName, final AppCompatActivity appCompatActivity, final float versionName, final String updateUrl) {
 
-        //TODO : Use Alert Dialog Utils
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(appCompatActivity);
-        builder1.setMessage("New version is available, please update...").setCancelable(false).setPositiveButton("Update", (dialog, id) -> ApplicationVCSUtils.downloadAndInstallApk(applicationName, versionName, updateUrl, appCompatActivity));
-        AlertDialog alert = builder1.create();
-        alert.setTitle("Warning!");
-        alert.show();
+        new AlertDialogUtils(
+                (dialog, which) -> ApplicationVCSUtils.downloadAndInstallApk(applicationName, versionName, updateUrl, appCompatActivity),
+                null
+        ).titledSingleButtonDialogue(appCompatActivity, "Warning!", "New version is available, please update...", "Update", false);
     }
 }
